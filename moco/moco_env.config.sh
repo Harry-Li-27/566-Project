2d8bf071a6b3d70b62ea9506590c1f0f28167e4b
woshizhouyihao <yihaozho@usc.edu>
2023-03-26 00:00:52 -0700

    Yihao/moco (#2)
    * feat: moco run
    
    * feat: moco init

---
+++ moco/moco_env.config.sh
@@ -0,0 +1,10 @@
+# conda 配置
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+conda install tensorboard
+
+pip install tensorboard
+pip install timm
+
+# pip uninstall setuptools
+pip install setuptools==59.5.0
