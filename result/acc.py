2546ccec70853279187a2d1b9e20e1885d7d9fef
yihao <haohaozhoujim@gmail.com>
2023-04-03 21:37:52 -0700

    feat: dino result script

--- result/acc.py
+++ result/acc.py
@@ -1,14 +1,24 @@
-# python acc.py <filename> > r.out
+# python acc.py <model> <filename> > r.out
 
 import sys
 import re
 
-log_file = sys.argv[1]
-acc_pattern = re.compile(r"\*\sAcc@1\s+([\d\.]+)")
+model = sys.argv[1]
+log_file = sys.argv[2]
 
-with open(log_file) as f:
-    for line in f:
-        match = acc_pattern.search(line)
-        if match:
-            acc = float(match.group(1))
-            print(acc)
+if model == "moco":
+    acc_pattern = re.compile(r"\*\sAcc@1\s+([\d\.]+)")
+    with open(log_file) as f:
+        for line in f:
+            match = acc_pattern.search(line)
+            if match:
+                acc = float(match.group(1))
+                print(acc)
+elif model == "dino":
+    acc_pattern = re.compile(r"\"test_acc1\":\s+([\d\.]+)")
+    with open(log_file) as f:
+        for line in f:
+            match = acc_pattern.search(line)
+            if match:
+                acc = float(match.group(1))
+                print(acc)
